b4c1bf22fd42d2f09b697aeeeba0f872a00fd5d0
kestrelmacknight-oss <kestrel.macknight@gmail.com>
2026-07-05 21:10:52 +0000

    Add gallery to channel type validation

--- lib/koda/servers/channel.ex
+++ lib/koda/servers/channel.ex
@@ -1,10 +1,8 @@
 defmodule Koda.Servers.Channel do
   use Ecto.Schema
   import Ecto.Changeset
-
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
-
   schema "channels" do
     field :name,               :string
     field :type,               :string, default: "text"
@@ -16,16 +14,16 @@ defmodule Koda.Servers.Channel do
     belongs_to :category, Koda.Servers.Category
     timestamps(type: :utc_datetime)
   end
-
   def changeset(channel, attrs) do
     channel
     |> cast(attrs, [:name, :type, :description, :position,
                     :is_subscriber_only, :is_read_only, :server_id, :category_id])
     |> validate_required([:name, :server_id])
     |> validate_length(:name, min: 1, max: 100)
-    |> validate_inclusion(:type, ["text", "voice"])
+    |> validate_inclusion(:type, ["text", "voice", "gallery"])
   end
-
   def voice?(%__MODULE__{type: "voice"}), do: true
   def voice?(_), do: false
+  def gallery?(%__MODULE__{type: "gallery"}), do: true
+  def gallery?(_), do: false
 end
