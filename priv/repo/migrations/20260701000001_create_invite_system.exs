50718252af3ed460aca3917c45430e6ee2636c4c
kestrelmacknight-oss <kestrel.macknight@gmail.com>
2026-07-13 03:57:59 +0000

    Use create_if_not_exists in invite system migration

--- priv/repo/migrations/20260701000001_create_invite_system.exs
+++ priv/repo/migrations/20260701000001_create_invite_system.exs
@@ -11,7 +11,7 @@ defmodule Koda.Repo.Migrations.CreateInviteSystem do
 
     # Server invite codes -- short random codes that let anyone join
     # a specific server. Optional expiry and use limit.
-    create table(:invites, primary_key: false) do
+    create_if_not_exists table(:invites, primary_key: false) do
       add :id,         :binary_id, primary_key: true
       add :code,       :string,    null: false
       add :server_id,  references(:servers, type: :binary_id, on_delete: :delete_all), null: false
@@ -22,12 +22,12 @@ defmodule Koda.Repo.Migrations.CreateInviteSystem do
       timestamps(type: :utc_datetime)
     end
 
-    create unique_index(:invites, [:code])
-    create index(:invites, [:server_id])
+    create_if_not_exists unique_index(:invites, [:code])
+    create_if_not_exists index(:invites, [:server_id])
 
     # Backer/redemption codes -- admin-generated codes that apply
     # account flags when redeemed. One use per code per user.
-    create table(:backer_codes, primary_key: false) do
+    create_if_not_exists table(:backer_codes, primary_key: false) do
       add :id,          :binary_id, primary_key: true
       add :code,        :string, null: false
       add :flags,       :map, default: %{}, null: false  # flags to apply on redemption
@@ -39,17 +39,17 @@ defmodule Koda.Repo.Migrations.CreateInviteSystem do
       timestamps(type: :utc_datetime)
     end
 
-    create unique_index(:backer_codes, [:code])
+    create_if_not_exists unique_index(:backer_codes, [:code])
 
     # Track which users have redeemed which backer codes
-    create table(:backer_code_redemptions, primary_key: false) do
+    create_if_not_exists table(:backer_code_redemptions, primary_key: false) do
       add :id,      :binary_id, primary_key: true
       add :code_id, references(:backer_codes, type: :binary_id, on_delete: :delete_all), null: false
       add :user_id, references(:users, type: :binary_id, on_delete: :delete_all), null: false
       add :redeemed_at, :utc_datetime, null: false
     end
 
-    create unique_index(:backer_code_redemptions, [:code_id, :user_id])
-    create index(:backer_code_redemptions, [:user_id])
+    create_if_not_exists unique_index(:backer_code_redemptions, [:code_id, :user_id])
+    create_if_not_exists index(:backer_code_redemptions, [:user_id])
   end
 end
